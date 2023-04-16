db909c0b0182630d89d387d485324f67a6ba83cd
AhtritCodes <tirtha13july2002sonofmp@gmail.com>
2023-04-17 00:17:50 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,73 @@
+const buttons = document.querySelectorAll('.calc-keys');
+const clearBtn = document.querySelector('.clear-btn');
+const equalBtn = document.querySelector('.equal-btn');
+const operand = document.querySelector('.operand');
+const result = document.querySelector('.result');
+
+let output = '';
+let string;
+let opr = '';
+let num = '';
+
+
+Array.from(buttons).forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        string = e.target.innerHTML;
+        if (string == '=') {
+            evaluate(opr);
+            result.innerHTML = `${output}`;
+        } 
+        else if (string == '+' || string == '-' || string == '%') {
+            opr = string;
+            output += opr;
+            operand.innerHTML = `${output}`;
+        } 
+        else if (string == 'x') {
+            opr = '*';
+            output += opr;
+            operand.innerHTML = `${output}`;
+        }
+        else if (string == '÷') {
+            opr = '/';
+            output += opr;
+            operand.innerHTML = `${output}`;
+        }
+        else if (string == 'AC') {
+            output = '';
+            operand.innerHTML = `${output}`;
+            result.innerHTML = `${output}`;
+        }
+        else {
+            num = e.target.innerHTML;
+            output += num;
+            operand.innerHTML = `${output}`;
+        }
+    });
+});
+
+const evaluate = (opr) => {
+    if (opr == '') return;
+    try {
+        switch (opr) {
+            case '+':
+                output = eval(output);
+                break;
+            case '-':
+                output = eval(output);
+                break;
+            case '*':
+                output = eval(output);
+                break;
+            case '/':
+                output = eval(output);
+                break;
+            case '%':
+                output = eval(output);
+                break;
+            default:
+                break;
+        }
+    } catch (error) {
+        alert('invalid operator / input');
+    }
+};
